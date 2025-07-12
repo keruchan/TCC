d98a344a3e2265f3b47f9b73a7593cef4c643078
keruchan <joshleones27@gmail.com>
2025-07-13 00:16:37 +0800

    Initial commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,9 @@
+ <div class="row">
+                        <div class="col-lg-12">
+                            <div class="footer">
+                            <p>
+  Balian Community College
+  <a href="https://web.facebook.com/bccfreeeducation" target="_blank" rel="noopener noreferrer">(BCC)</a>
+</p>                            </div>
+                        </div>
+                    </div>
